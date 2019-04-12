7d821f2ad1ae05d0991268037807ebe11e7d0fac
Md. Khaleduzzaman <khaled@DESKTOP-ROGSLMC.localdomain>
2019-04-12 10:32:26 +0600

    Task-3: Collect data from user to back-end

--- Khaled/routes/users.js
+++ Khaled/routes/users.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/collect_data', function(req, res, next) {
+  res.render('collect_data');
+});
+
+router.post('/collect_data', function(req, res, next) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  console.log('username: ' + username + '\n' +
+              'email: ' + email + '\n' +
+              'password: ' + password + '\n' +
+              'password2: ' + password2 + '\n');
+
+  res.send('hello');
+});
+
 module.exports = router;
